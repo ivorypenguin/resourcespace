c6589ad612996bc6bb8f6db6f76a3c582b518234
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-10-18 09:07:26 +0000

    Added line I somehow dropped from view.php, breaking find similar functionality
    git-svn-id: http://svn.montala.net/svn/resourcespace@3837 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/view.php
+++ pages/view.php
@@ -1152,7 +1152,7 @@ for ($n=0;$n<count($keywords);$n++)
 <div class="clearerleft"> </div>
 <br />
 <input name="search" type="submit" value="&nbsp;&nbsp;<?php echo $lang["searchbutton"]?>&nbsp;&nbsp;" id="dosearch"/>
-
+<iframe src="blank.html" frameborder=0 scrolling=no width=1 height=1 style="visibility:hidden;" name="resultcount" id="resultcount"></iframe>
 </form>
 <div class="clearerleft"> </div>
 </div>
