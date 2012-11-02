6f6b6542a9e89aa16cb84edba393ef61218bbb82
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-11-02 19:34:17 +0000

    added the tag Smart Collection to distinguish smart collections in gallery lists
    git-svn-id: http://svn.montala.net/svn/resourcespace@3915 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/themes.php
+++ pages/themes.php
@@ -173,7 +173,8 @@ function DisplayTheme($themes=array())
 			{
 			?>
 			<tr <?php hook("collectionlistrowstyle");?>>
-			<td width="50%"><div class="ListTitle"><a href="<?php echo $baseurl_short?>pages/search.php?search=!collection<?php echo $getthemes[$m]["ref"]?>&bc_from=themes"  title="<?php echo $lang["collectionviewhover"]?>" onClick="return CentralSpaceLoad(this,true);"><?php echo htmlspecialchars(i18n_get_translated($getthemes[$m]["name"]))?></a>
+				<?php if (!isset($getthemes[$m]['savedsearch'])||(isset($getthemes[$m]['savedsearch'])&&$getthemes[$m]['savedsearch']==null)){$collection_tag="";} else {$collection_tag=$lang['smartcollection'].": ";}?>
+			<td width="50%"><div class="ListTitle"><a href="<?php echo $baseurl_short?>pages/search.php?search=!collection<?php echo $getthemes[$m]["ref"]?>&bc_from=themes"  title="<?php echo $lang["collectionviewhover"]?>" onClick="return CentralSpaceLoad(this,true);"><?php echo $collection_tag.htmlspecialchars(i18n_get_translated($getthemes[$m]["name"]))?></a>
 			<?php if ($flag_new_themes && (time()-strtotime($getthemes[$m]["created"]))<(60*60*24*14)) { ?><div class="NewFlag"><?php echo $lang["newflag"]?></div><?php } ?>
 			</div></td>
 			<?php if ($themes_ref_column){?>
