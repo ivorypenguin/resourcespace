6fd0372c374b03424d0e4538fc07d6ef9bb3d6bb
alexey <alexey@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-11-04 00:44:29 +0000

    fix action-select not to lose permissions for collections frame
    git-svn-id: http://svn.montala.net/svn/resourcespace@3930 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/collection_manage.php
+++ pages/collection_manage.php
@@ -339,7 +339,7 @@ if (!hook('collectionaccessmode')) {
         <?php if ($collections_compact_style){
         include("collections_compact_style.php"); } else {
 ?><a onClick="return CentralSpaceLoad(this,true);" href="<?php echo $baseurl_short?>pages/search.php?search=<?php echo urlencode("!collection" . $collections[$n]["ref"])?>">&gt;&nbsp;<?php echo $lang["viewall"]?></a>
-	&nbsp;<a <?php if ($frameless_collections && !checkperm("b")){ ?>href="#" onclick="ChangeCollection(<?php echo $collections[$n]["ref"]?>);"
+	&nbsp;<a <?php if ($frameless_collections && !checkperm("b")){ ?>href="#" onclick="ChangeCollection(<?php echo $collections[$n]["ref"]?>, '');"
 		<?php } elseif ($autoshow_thumbs) {?>onclick=" top.document.getElementById('topframe').rows='*<?php if ($collection_resize!=true) {?>,3<?php } ?>,138'; return true;"
 		href="collections.php?collection=<?php echo $collections[$n]["ref"]?>&amp;thumbs=show" target="collections"
 		<?php } else {?>href="collections.php?collection=<?php echo $collections[$n]["ref"]?>" target="collections"<?php }?>>&gt;&nbsp;<?php echo $lang["action-select"]?></a>
