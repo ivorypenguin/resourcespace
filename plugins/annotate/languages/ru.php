6200d920516049509ae6039eb359126916616c27
alexey <alexey@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-11-05 18:12:55 +0000

    Russian language for annotate plugin
    git-svn-id: http://svn.montala.net/svn/resourcespace@3936 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/annotate/languages/ru.php
@@ -0,0 +1,31 @@
+<?php
+# Russian
+# Language File for the Annotate Plugin
+# -------
+#
+#
+$lang['annotations']="Аннотации";
+$lang['pdfwithnotes']="PDF с аннотациями";
+$lang["annotatepdfconfig"]="PDF с аннотациями";
+$lang["annotatepdfintrotext"]="Выбрать размер бумаги для PDF с аннотациями.";
+
+$lang["error-saving"]="Ошибка при записи аннотации.";
+$lang["error-deleting"]="Ошибка при удалении аннотации.";
+
+$lang["action-add_note"]="Добавить аннотацию";
+$lang["action-toggle-on"]="Показать все";
+$lang["action-toggle-off"]="Спрятать все";
+$lang["note-1"]="1 аннотация";
+$lang["note-2"]="%number аннотаций"; # %number will be replaced, e.g. 3 notes
+
+$lang["annotate_configuration"]="Настройки аннотаций";
+$lang["extensions_to_exclude"]="Исключить разрешения<br />(разделяя запятыми):";
+$lang["resource_types_to_exclude"]="Исключить типы объектов<br />(отметить):";
+$lang["annotatedebug"]="Debug:";
+$lang["toggle"]="Вкл/Выкл";
+$lang["annotate_public_view"] = "Публичны ли аннотации (например, когда делятся коллекциями)?";
+$lang["annotate_show_author"] = "Показывать автора аннотации?";
+
+$lang["annotate_font"]="Шрифт:";
+$lang["annotate_pdf_output"]="Вывод в PDF (экспериментальная опция)";
+$lang["annotate_pdf_output_only_annotated"]="Включать в PDF только аннотированные объекты";
