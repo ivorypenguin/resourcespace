76ce8468ba6f08906e778c7d7539af59c0e4ec94
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-11-06 16:05:38 +0000

    Fix for theme categroy editing
    git-svn-id: http://svn.montala.net/svn/resourcespace@3961 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/theme_edit.php
+++ pages/theme_edit.php
@@ -16,15 +16,15 @@ function save_themename()
 
 $themes=array();
 $themecount=0;
-foreach ($_GET as $key => $value) {
+reset($_POST);reset($_GET);foreach (array_merge($_GET, $_POST) as $key=>$value) {
 	// only set necessary vars
 	if (substr($key,0,5)=="theme" && $value!=""){
 		$themes[$themecount]=urldecode($value);
-		//echo $key . $value . "<br>";
 		$themecount++;		
 		}	
 	}
-	
+
+
 # Work out theme name and level, also construct back link
 $link="themes.php?";
 $lastlevelchange=getvalescaped("lastlevelchange",1);
@@ -32,29 +32,45 @@ $link.="lastlevelchange=" . $lastlevelchange . "&";
 for ($x=0;$x<$themecount;$x++)
 	{		
 	if (!$x==0){$link.="&";}		
-	$link.="theme";	
 	If ($x==0)
 		{
-		$collection_column="theme";		
-		$link.= "=";		
-		}
-	else
-		{
-		$collection_column="theme" . ($x+1);		
-		$link.=$x."=";		
-		}
-	
-	if (isset($themes[$x])&&!isset($themes[$x+1]))
-		{
+		$collection_column="theme";	
+		if ($x<$themecount-1)
+			{$link.= "theme=" . urlencode($themes[$x]);}
+		elseif ($x==$themecount-1 && getval("rename","")!="" && !($themes_category_split_pages)) #add  new name of theme to back link
+			{
+			$link.= "theme=" . getvalescaped("rename",""); 
+			}
 		$themename=i18n_get_translated($themes[$x]);
-		if(!($themes_category_split_pages))
-			{$link.=urlencode($themes[$x]);}
 		}
 	else
 		{
-		$link.=urlencode($themes[$x]);			
-		}
+		$collection_column="theme" . ($x+1);
+		if ($x<$themecount-1 || getval("rename","")=="") #add current theme to back link only if not renaming it
+			{$link.= "theme" . ($x+1) . "=" . urlencode($themes[$x]);}	
+		elseif ($x==$themecount-1 && getval("rename","")!="" && !($themes_category_split_pages)) #add  new name of theme to back link
+			{
+			$link.= "theme" . ($x+1) . "=" . getvalescaped("rename",""); 
+			}
+		$themename=i18n_get_translated($themes[$x]);
+		}	
 	}
+		//if (isset($themes[$x])&&!isset($themes[$x+1]))
+			//{			
+			//if(!($themes_category_split_pages))
+				//{$link.=urlencode($themes[$x]);}
+			//}
+		
+					
+		
+
+		
+	//else
+		//{
+		//$collection_column="theme" . ($x+1);
+		//$themename=i18n_get_translated($themes[$x]);
+		//}
+	//}
 	
 if (getval("rename","")!="")
 	{
@@ -79,6 +95,26 @@ if (!checkperm("t")) {
 <h1><?php echo $lang["action-edit"] . " " . $lang["themecategory"]?></h1>
 <p><?php echo text("introtext")?></p>
 	<form method=post id="themeform" action="<?php echo $baseurl_short?>pages/theme_edit.php">
+		<input type="hidden" name="collection_column" id="collection_column" value="<?php echo $collection_column?>">
+		<input type="hidden" name="link" id="link" value="<?php echo $link?>">
+		<input type="hidden" name="lastlevelchange" id="lastlevelchange" value="<?php echo $lastlevelchange?>">
+		
+		<?php
+		for ($x=0;$x<$themecount;$x++)
+			{
+			if ($x==0)
+				{?>
+				<input type="hidden" name="theme" id="theme" value="<?php echo i18n_get_translated($themes[$x])?>">
+				<?php
+				}
+			else
+				{ ?>
+				<input type="hidden" name="theme<?php echo $x+1 ?>" id="theme<?php echo $x+1 ?>" value="<?php echo i18n_get_translated($themes[$x]) ?>">	
+				<?php }					
+			}
+		
+		?>
+		
 		<div class="Question">
 			<label for="rename"><?php echo $lang["name"]?></label><input type=text class="stdwidth" name="rename" id="rename" value="<?php echo $themename?>" maxlength="100" />
 			<div class="clearerleft"> </div>
