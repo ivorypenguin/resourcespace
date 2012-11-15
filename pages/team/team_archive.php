1d0eca77cb6f4e8b5436768acd34bb705c4ef11f
hfrizen <hfrizen@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-11-15 23:02:12 +0000

    Added CentralSpaceLoad to one more link in Manage Archive Resources.
    git-svn-id: http://svn.montala.net/svn/resourcespace@4013 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/team/team_archive.php
+++ pages/team/team_archive.php
@@ -23,7 +23,7 @@ include "../../include/header.php";
 	</ul>
 	</div>
 	
-	<p><a href="<?php echo $baseurl_short?>pages/team/team_home.php">&gt;&nbsp;<?php echo $lang["backtoteamhome"]?></a></p>
+	<p><a href="<?php echo $baseurl_short?>pages/team/team_home.php" onClick="return CentralSpaceLoad(this,true);">&gt;&nbsp;<?php echo $lang["backtoteamhome"]?></a></p>
   </div>
 
 <?php
