3ae71fd5d8e355c5d1987c6849ecf6b3aef33182
hfrizen <hfrizen@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-12-05 13:09:35 +0000

    Minor bugfixes in edit.php.
    Fixes issue #212.
    
    
    git-svn-id: http://svn.montala.net/svn/resourcespace@4073 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/edit.php
+++ pages/edit.php
@@ -392,7 +392,7 @@ if ($resource["file_extension"]!="") { ?><strong><?php echo str_replace_formatte
 
 <?php if (!checkperm("F*")) { ?>
 <div class="Question" id="question_imagecorrection">
-<label><?php echo $lang["imagecorrection"]?><br/><?php echo $lang["previewthumbonly"]?></label><select class="stdwidth" name="tweak" id="tweak" onChange="document.getElementById('mainform').submit();">
+<label><?php echo $lang["imagecorrection"]?><br/><?php echo $lang["previewthumbonly"]?></label><select class="stdwidth" name="tweak" id="tweak" onChange="CentralSpacePost(document.getElementById('mainform'),true);">
 <option value=""><?php echo $lang["select"]?></option>
 <?php if ($resource["has_image"]==1) { ?>
 <?php
@@ -491,7 +491,7 @@ if ($camera_autorotation_checked) {echo ' checked';}?>>
 <div class="Question" id="question_resourcetype">
 <label for="resourcetype"><?php echo $lang["resourcetype"]?></label>
 <select name="resource_type" id="resourcetype" class="stdwidth" 
-onChange="<?php if ($ref>0) { ?>if (confirm('<?php echo $lang["editresourcetypewarning"]; ?>')){<?php } ?>document.getElementById('mainform').submit();<?php if ($ref>0) { ?>}else {return}<?php } ?>">
+onChange="<?php if ($ref>0) { ?>if (confirm('<?php echo $lang["editresourcetypewarning"]; ?>')){<?php } ?>CentralSpacePost(document.getElementById('mainform'),true);<?php if ($ref>0) { ?>}else {return}<?php } ?>">
 <?php
 $types=get_resource_types();
 for ($n=0;$n<count($types);$n++)
