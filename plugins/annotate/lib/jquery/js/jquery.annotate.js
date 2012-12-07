d5d7324e45a8f465e31f5cd5fb8df8dc40261186
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-12-07 19:49:15 +0000

    fixes for annotate form positioning with ui layout
    git-svn-id: http://svn.montala.net/svn/resourcespace@4090 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/annotate/lib/jquery/js/jquery.annotate.js
+++ plugins/annotate/lib/jquery/js/jquery.annotate.js
@@ -335,34 +335,47 @@ function isInt(value) {
         var form = $('<div id="image-annotate-edit-form"><form><textarea id="image-annotate-text" name="text" rows="3" cols="30">' + this.note.text.replace(new RegExp('<br />', 'g'), '') + '</textarea></form></div>');
         this.form = form;
 
-        $('body').append(this.form);
-        this.form.css('left', this.area.offset().left + 'px');
-        this.form.css('top', (parseInt(this.area.offset().top) + parseInt(this.area.height()) + 7) + 'px');
-
+		//$('body').append(this.form);
+        	
         // Set the area as a draggable/resizable element contained in the image canvas.
         // Would be better to use the containment option for resizable but buggy
+		//this.form.css('left', this.area.offset().left + 'px');
+        //this.form.css('top', (parseInt(this.area.offset().top) + parseInt(this.area.height()) + 7) + 'px');
+
+		// modified for RS
+		$('.ui-layout-center').append(this.form);	
+		this.form.css('left', this.area.offset().left + $('.ui-layout-center').scrollLeft()+ 'px');
+        this.form.css('top', (parseInt(this.area.offset().top)+ $('.ui-layout-center').scrollTop() + parseInt(this.area.height()) + 7) + 'px');
+		this.form.css('z-index',3);
+		//
+		
         area.resizable({
             handles: 'all',
             resize: function(e, ui) {
-                form.css('left', area.offset().left + 'px');
-                form.css('top', (parseInt(area.offset().top) + parseInt(area.height()) + 7) + 'px');
+                form.css('left', area.offset().left  + $('.ui-layout-center').scrollLeft()+ 'px');
+                form.css('top', (parseInt(area.offset().top) + $('.ui-layout-center').scrollTop() + parseInt(area.height()) + 7) + 'px');
             },
             stop: function(e, ui) {
-                form.css('left', area.offset().left + 'px');
-                form.css('top', (parseInt(area.offset().top) + parseInt(area.height()) + 7) + 'px');
+                form.css('left', area.offset().left + $('.ui-layout-center').scrollLeft() + 'px');
+                form.css('top', (parseInt(area.offset().top) + $('.ui-layout-center').scrollTop() + parseInt(area.height()) + 7) + 'px');
             }
         })
         .draggable({
             containment: image.canvas,
             drag: function(e, ui) {
-                form.css('left', area.offset().left + 'px');
-                form.css('top', (parseInt(area.offset().top) + parseInt(area.height()) + 7) + 'px');
+                form.css('left', area.offset().left  + $('.ui-layout-center').scrollLeft()+ 'px');
+                form.css('top', (parseInt(area.offset().top) + $('.ui-layout-center').scrollTop() + parseInt(area.height()) + 7) + 'px');
             },
             stop: function(e, ui) {
-                form.css('left', area.offset().left + 'px');
-                form.css('top', (parseInt(area.offset().top) + parseInt(area.height()) + 7) + 'px');
+                form.css('left', area.offset().left + $('.ui-layout-center').scrollLeft() + 'px');
+                form.css('top', (parseInt(area.offset().top) + $('.ui-layout-center').scrollTop() + parseInt(area.height()) + 7) + 'px');
             }
         });
+        
+     
+        
+        
+        
         return this;
     };
 
