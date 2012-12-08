853f8f53aa668363eed9b4631a80b14efbccc4fa
hfrizen <hfrizen@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-12-08 16:42:00 +0000

    Bugfix for Resource of the Day plugin. The plugin now handles different sized previews. Fixed a related bug specific to GreySpace.
    Fixes issue #111.
    
    
    git-svn-id: http://svn.montala.net/svn/resourcespace@4093 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/resourceoftheday/hooks/home.php
+++ plugins/resourceoftheday/hooks/home.php
@@ -4,16 +4,25 @@ function HookResourceofthedayHomeReplaceslideshow ()
 	{
 	include_once dirname(__FILE__)."/../inc/rotd_functions.php";
 
-	global $baseurl;
+	global $baseurl, $view_title_field;
 
 	$rotd=get_resource_of_the_day();
 	if ($rotd===false) {return false;} # No ROTD, return false to disable hook and display standard slide show.
-	
-	# Fetch resource data
-	$resource=get_resource_data($rotd);
 
-	# Fetch title
-	$title=sql_value("select value from resource_data where resource='$rotd' and resource_type_field=8","");
+    # Get preview width
+    $sizes = get_image_sizes($rotd, true);
+    foreach ($sizes as $size)
+        {
+        if ($size["id"]=="pre")
+            {
+            $width = $size["width"];
+            break;
+            }
+        }
+
+
+    # Fetch title
+    $title = sql_value("select value from resource_data where resource='$rotd' and resource_type_field=$view_title_field","");
 
 	# Fetch caption
 	$caption=sql_value("select value from resource_data where resource='$rotd' and resource_type_field=18","");
@@ -21,7 +30,7 @@ function HookResourceofthedayHomeReplaceslideshow ()
 	# Show resource!
 	$pre=get_resource_path($rotd,false,"pre",false,"jpg");
 	?>
-	<div class="HomePicturePanel RecordPanel" style="width:350px;padding-left:4px;">
+	<div class="HomePicturePanel RecordPanel" style="width: <?php echo $width ?>px; padding-left: 3px;">
 	<a onClick="return CentralSpaceLoad(this,true);" href="<?php echo $baseurl?>/pages/view.php?ref=<?php echo $rotd ?>"><img class="ImageBorder" style="margin-bottom: 10px;" src="<?php echo $pre ?>" /></a>
 	<br />
 	<h2 ><?php echo i18n_get_translated(htmlspecialchars($title)) ?></h2>
