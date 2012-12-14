88d38c46d948b91cfd79cca6d13463bc2a24b7a7
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-12-14 11:09:09 +0000

    Added generic content.php which can be used to display any content e.g. additional info links
    git-svn-id: http://svn.montala.net/svn/resourcespace@4110 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ pages/content.php
@@ -0,0 +1,17 @@
+<?php
+include "../include/db.php";
+include "../include/general.php";
+include "../include/authenticate.php";
+include "../include/header.php";
+
+$content=getvalescaped("content","");
+if ($content!=""){$content=text($content);}else{$content="This is default content text. You can create text (including html) in Team Centre->Manage Content and display it here.";}
+?>
+
+<div class="BasicsBox"> 
+  <?php echo $content ?>
+</div>
+
+<?php
+include "../include/footer.php";
+?>
