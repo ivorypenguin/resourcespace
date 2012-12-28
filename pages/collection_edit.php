11012f8750b26264a511ce074ea301268d8e00e6
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2012-12-28 19:39:23 +0000

    ajax load changes to the theme in collection_edit...it's still not ideal as it actuall saves these changes, but this is how it has always worked, and this is just a fix for excessive reloading of the collection bar.
    git-svn-id: http://svn.montala.net/svn/resourcespace@4179 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/collection_edit.php
+++ pages/collection_edit.php
@@ -168,7 +168,7 @@ if ($theme_category_levels>=$i)
 	?>
 	<div class="Question">
 	<label for="theme<?php echo $themeindex?>"><?php echo $lang["themecategory"] . " ".$themeindex ?></label>
-	<?php if (count($themes)>0){?><select class="stdwidth" name="theme<?php echo $themeindex?>" id="theme<?php echo $themeindex?>" <?php if ($theme_category_levels>=$themeindex) { ?>onchange="if (document.getElementById('theme<?php echo $themeindex?>').value!=='') {document.getElementById('addlevel').value='yes'; document.getElementById('collectionform').submit();} else {document.getElementById('redirect').value='';document.getElementById('collectionform').submit();}"<?php } ?>><option value=""><?php echo $lang["select"]?></option>
+	<?php if (count($themes)>0){?><select class="stdwidth" name="theme<?php echo $themeindex?>" id="theme<?php echo $themeindex?>" <?php if ($theme_category_levels>=$themeindex) { ?>onchange="if (document.getElementById('theme<?php echo $themeindex?>').value!=='') {document.getElementById('addlevel').value='yes'; return CentralSpacePost(jQuery('#collectionform')[0])} else {document.getElementById('redirect').value='';return CentralSpacePost(jQuery('#collectionform')[0])}"<?php } ?>><option value=""><?php echo $lang["select"]?></option>
 	<?php 
 	$lastselected=false;
 	for ($n=0;$n<count($themes);$n++) { ?>
@@ -182,10 +182,10 @@ if ($theme_category_levels>=$i)
 		<?php } //end conditional selector?>
 	<input type=text class="medwidth" name="newtheme<?php echo $themeindex?>" id="newtheme<?php echo $themeindex?>" value="" maxlength="100">
 	<?php if ($themecount!=1){?>
-	<input type=button class="medcomplementwidth" value="<?php echo $lang['save'];?>" style="display:inline;" onclick="document.getElementById('addlevel').value='yes';document.getElementById('collectionform').submit();"/>	
+	<input type=button class="medcomplementwidth" value="<?php echo $lang['save'];?>" style="display:inline;" onclick="document.getElementById('addlevel').value='yes';return CentralSpacePost(jQuery('#collectionform')[0])"/>	
 	<?php } ?>
 	<?php if ($themecount==1){?>
-	<input type=button class="medcomplementwidth" value="<?php echo $lang['add'];?>" style="display:inline;" onclick="if (document.getElementById('newtheme<?php echo $themeindex?>').value==''){alert('<?php echo $lang["collectionsnothemeselected"] ?>');return false;}document.getElementById('addlevel').value='yes';document.getElementById('collectionform').submit();"/><?php }?>
+	<input type=button class="medcomplementwidth" value="<?php echo $lang['add'];?>" style="display:inline;" onclick="if (document.getElementById('newtheme<?php echo $themeindex?>').value==''){alert('<?php echo $lang["collectionsnothemeselected"] ?>');return false;}document.getElementById('addlevel').value='yes';return CentralSpacePost(jQuery('#collectionform')[0])"/><?php }?>
 	<div class="clearerleft"> </div>
 	</div>
 	<?php
