d5a269d3b477b8770172e8e60cec53609b962720
danh <danh@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2013-02-26 16:19:20 +0000

    Allowed more room for question labels as browser winders are generally wider than they were in 2006. Fixed the display for date range searches - the standard label being reused was too wide.
    git-svn-id: http://svn.montala.net/svn/resourcespace@4324 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/search_advanced.php
+++ pages/search_advanced.php
@@ -178,7 +178,7 @@ for ($n=0;$n<count($types);$n++)
 if ($daterange_search)
 	{ ?>
 	<!--  date range search -->
-	<div><label><?php echo $lang["fromdate"]?></label>
+	<div><label class="InnerLabel"><?php echo $lang["fromdate"]?></label>
 	<select name="startyear" class="SearchWidth" style="width:100px;" onChange="UpdateResultCount();">
 	  <option value=""><?php echo $lang["anyyear"]?></option>
 	  <?php
@@ -210,7 +210,7 @@ if ($daterange_search)
 	  ?>
 	</select>
 	<!-- end date -->	
-	</div><br><div><label></label><label><?php echo $lang["todate"]?></label>
+	</div><br><div><label></label><label class="InnerLabel"><?php echo $lang["todate"]?></label>
 	<select name="endyear" class="SearchWidth" style="width:100px;" onChange="UpdateResultCount();">
 	  <option value=""><?php echo $lang["anyyear"]?></option>
 	  <?php
