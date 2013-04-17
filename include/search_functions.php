c2a2996cdb2f63e6f9c39d9fbde94d8ee177a028
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2013-04-17 20:14:43 +0000

    fix for empty field search, did not recognize empty fixed list entries (',') as empty
    git-svn-id: http://svn.montala.net/svn/resourcespace@4401 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/search_functions.php
+++ include/search_functions.php
@@ -907,7 +907,7 @@ function do_search($search,$restypes="",$order_by="relevance",$archive=0,$fetchr
 		if (!is_numeric($nodatafield)){$nodatafield=sql_value("select ref value from resource_type_field where name='$nodatafield'","");}
 		if ($nodatafield==""){exit('invalid !empty search');}
 	
-		return sql_query("$sql_prefix select distinct r.hit_count score,$select from resource r left outer join resource_data rd on r.ref=rd.resource and rd.resource_type_field='$nodatafield' $sql_join where (rd.value ='' or rd.value is null)  and $sql_filter group by r.ref order by $order_by $sql_suffix");
+		return sql_query("$sql_prefix select distinct r.hit_count score,$select from resource r left outer join resource_data rd on r.ref=rd.resource and rd.resource_type_field='$nodatafield' $sql_join where (rd.value ='' or rd.value is null or rd.value=',')  and $sql_filter group by r.ref order by $order_by $sql_suffix");
 		}
 	
 	# Search for a list of resources
