779cd49ac6d6fc959d667e24f00a0f696d07aa6a
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2013-04-24 10:20:34 +0000

    Fixed inability to name new collection at upload when upload_add_to_new_collection was set to false but upload_add_to_new_collection_opt was true
    git-svn-id: http://svn.montala.net/svn/resourcespace@4419 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/edit.php
+++ pages/edit.php
@@ -1249,7 +1249,7 @@ if ($ref<0)
 	
 		<div class="clearerleft"> </div>
 		<div name="collectioninfo" id="collectioninfo" style="display:none;">
-		<div name="collectionname" id="collectionname" <?php if ($upload_add_to_new_collection && $upload_add_to_new_collection_opt){ ?> style="display:block;"<?php } else { ?> style="display:none;"<?php } ?>>
+		<div name="collectionname" id="collectionname" <?php if ($upload_add_to_new_collection_opt){ ?> style="display:block;"<?php } else { ?> style="display:none;"<?php } ?>>
 		<label for="collection_add"><?php echo $lang["collectionname"]?><?php if ($upload_collection_name_required){?><sup>*</sup><?php } ?></label>
 		<input type=text id="entercolname" name="entercolname" class="stdwidth" value='<?php echo htmlentities(stripslashes(getval("entercolname","")), ENT_QUOTES);?>'> 
 		
