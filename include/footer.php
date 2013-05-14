88e7e46964d9ba9def50ac438dffd93d683bb935
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2013-05-14 06:55:45 +0000

    Fix for external shares broken in 4454
    git-svn-id: http://svn.montala.net/svn/resourcespace@4462 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/footer.php
+++ include/footer.php
@@ -291,10 +291,10 @@ function setContent() {
 		south__onclose_start: function(pane){
 			if (pane=="south"){
 			if(jQuery('.ui-layout-south').height()>=<?php echo $collection_frame_height?> && thumbs!="hide"){
-				CollectionDivLoad('<?php echo $baseurl_short?>pages/collections.php?thumbs=hide&collection='+usercollection+'<?php echo urlencode((isset($k)) ? "&k=$k" : ""); ?>');
+				CollectionDivLoad('<?php echo $baseurl_short?>pages/collections.php?thumbs=hide&collection='+usercollection+'<?php echo (isset($k) ? "&k=".urlencode($k) : ""); ?>');
 				document.cookie = "thumbs=hide";
 			} else if(jQuery('.ui-layout-south').height()==40 && thumbs=="hide"){
-				CollectionDivLoad('<?php echo $baseurl_short?>pages/collections.php?thumbs=show&collection='+usercollection+'<?php echo urlencode((isset($k)) ? "&k=$k" : ""); ?>');
+				CollectionDivLoad('<?php echo $baseurl_short?>pages/collections.php?thumbs=show&collection='+usercollection+'<?php echo (isset($k) ? "&k=".urlencode($k) : ""); ?>');
 				document.cookie = "thumbs=show";
 			}
 			return false;
@@ -304,10 +304,10 @@ function setContent() {
 			if (pane=="south"){
 			if(jQuery('.ui-layout-south').height()<<?php echo $collection_frame_height?> && thumbs!="hide"){
 				document.cookie = "thumbs=hide";
-				CollectionDivLoad('<?php echo $baseurl_short?>pages/collections.php?thumbs=hide&collection='+usercollection+'<?php echo urlencode((isset($k)) ? "&k=$k" : ""); ?>');
+				CollectionDivLoad('<?php echo $baseurl_short?>pages/collections.php?thumbs=hide&collection='+usercollection+'<?php echo (isset($k) ? "&k=".urlencode($k) : ""); ?>');
 			} else if(jQuery('.ui-layout-south').height()>40 && thumbs=="hide"){
 				document.cookie = "thumbs=show";
-				CollectionDivLoad('<?php echo $baseurl_short?>pages/collections.php?thumbs=show&collection='+usercollection+'<?php echo urlencode((isset($k)) ? "&k=$k" : ""); ?>');
+				CollectionDivLoad('<?php echo $baseurl_short?>pages/collections.php?thumbs=show&collection='+usercollection+'<?php echo (isset($k) ? "&k=".urlencode($k) : ""); ?>');
 			}return false;
 			}
 		}
@@ -318,7 +318,7 @@ function setContent() {
 }
 
 window.onload = function() {
-    setContent(); CollectionDivLoad('<?php echo $baseurl_short?>pages/collections.php?thumbs=<?php echo urlencode($thumbs); ?>&collection='+usercollection+'<?php echo urlencode((isset($k)) ? "&k=$k" : ""); ?>');}
+    setContent(); CollectionDivLoad('<?php echo $baseurl_short?>pages/collections.php?thumbs=<?php echo urlencode($thumbs); ?>&collection='+usercollection+'<?php echo (isset($k) ? "&k=".urlencode($k) : ""); ?>');}
 </script>
 <?php } // end omit_collectiondiv_load_pages ?>	
 
