7e43b6696dd8512b2daf655cdd7b1693c30b4f39
alexey <alexey@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2013-07-01 16:25:43 +0000

    get star search value from cookie when needed
    git-svn-id: http://svn.montala.net/svn/resourcespace@4561 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/ajax/reload_searchbar.php
+++ pages/ajax/reload_searchbar.php
@@ -3,6 +3,7 @@
 # pull values from cookies if necessary, for non-search pages where this info hasn't been submitted
 if (!isset($restypes)) {$restypes=@$_COOKIE["restypes"];}
 if (!isset($search) || ((strpos($search,"!")!==false))) {$quicksearch=(isset($_COOKIE["search"])?$_COOKIE["search"]:"");} else {$quicksearch=$search;}
+if (!isset($starsearch) || ((strpos($starsearch,"!")!==false))) {$starsearch=(isset($_COOKIE["starsearch"])?$_COOKIE["starsearch"]:"");} 
 
 include_once('../../include/db.php');
 include_once('../../include/general.php');
