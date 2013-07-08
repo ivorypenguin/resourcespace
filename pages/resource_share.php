68f7aa48ac60832944938d23b87afb28355ce5f3
sbithell <sbithell@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2013-07-08 15:04:27 +0000

    Added resource share pages to replace previous link function. Better base for future resource sharing functions.
    git-svn-id: http://svn.montala.net/svn/resourcespace@4589 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ pages/resource_share.php
@@ -0,0 +1,49 @@
+<?php
+include "../include/db.php";
+include "../include/authenticate.php";
+include "../include/general.php";
+include "../include/search_functions.php";
+include "../include/resource_functions.php";
+include "../include/collections_functions.php";
+
+$ref=getvalescaped ("ref","",true);
+# fetch the current search (for finding simlar matches)
+$search=getvalescaped("search","");
+$order_by=getvalescaped("order_by","relevance");
+$offset=getvalescaped("offset",0,true);
+$restypes=getvalescaped("restypes","");
+if (strpos($search,"!")!==false) {$restypes="";}
+$archive=getvalescaped("archive",0,true);
+$starsearch=getvalescaped("starsearch","");
+$default_sort="DESC";
+if (substr($order_by,0,5)=="field"){$default_sort="ASC";}
+$sort=getval("sort",$default_sort);
+?>
+
+
+<?php
+include "../include/header.php";
+?>
+<div class="BasicsBox">
+<p><a href="<?php echo $baseurl_short?>pages/view.php?ref=<?php echo urlencode($ref)?>&search=<?php echo urlencode($search)?>&offset=<?php echo urlencode($offset)?>&order_by=<?php echo urlencode($order_by) ?>&sort=<?php echo urlencode($sort)?>&archive=<?php echo urlencode($archive)?>"  onClick="return CentralSpaceLoad(this,true);">&lt;&nbsp;<?php echo $lang["backtoresourceview"]?></a></p>
+
+<h1>Share Resource</h1>
+
+
+<div class="BasicsBox"> 
+
+<p><?php echo $lang["generateurlinternal"]?></p>
+
+
+<p><input class="URLDisplay" type="text" value="<?php echo $baseurl?>/?r=<?php echo $ref?>">
+	
+
+
+
+</div>
+
+
+<?php
+
+include "../include/footer.php";
+?>
