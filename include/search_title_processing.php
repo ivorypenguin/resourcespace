c43263f6238521d28f510965e459640cb3e078c6
alexey <alexey@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2013-07-18 23:13:32 +0000

    ADD - show contributed by <user fullname> in !contributions search
    git-svn-id: http://svn.montala.net/svn/resourcespace@4648 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/search_title_processing.php
+++ include/search_title_processing.php
@@ -210,6 +210,11 @@ if ($search_titles)
                     break;
                 }
             }
+            else 
+            {
+            $udata=get_user($cuser);
+            $search_title = '<h1 class="searchcrumbs"><a href='.$baseurl_short.'pages/search.php?search=!contributions'.$cuser.$parameters_string.' onClick="return CentralSpaceLoad(this,true);">'.$lang["contributedby"]." ".htmlspecialchars($udata["fullname"])." - ".$lang["status".intval($archive)].'</a>'.$searchcrumbs.'</h1> ';
+            }
         }
     else if ($archive!=0)
         {
