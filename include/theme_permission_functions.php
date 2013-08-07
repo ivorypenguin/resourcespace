95dfc0f14ad1c6aee420a54727b3dc397e33e688
mhinton <mhinton@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2013-08-07 11:26:32 +0000

    CHG - updated to support j*, j<top category>, j-<sub category deny> permission directives
    git-svn-id: http://svn.montala.net/svn/resourcespace@4766 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/theme_permission_functions.php
+++ include/theme_permission_functions.php
@@ -2,22 +2,10 @@
 
 // ---------- start of n-level theme permissions ----------
 
-# used to store black-list of collections which user is not allowed to view via themes (because of "j*" permissions)
-$current_user_collection_blacklisted_no_perms = array();
-
-# returns TRUE only if user is allowed to view collection as restricted by "j*" permissions for theme
-#
-function checkThemePerm($collectionRef) 
-	{	
-	global $current_user_collection_blacklisted_no_perms;
-	if (empty($current_user_collection_blacklisted_no_perms)) getThemePathPerms();
-	return (!in_array($collectionRef, $current_user_collection_blacklisted_no_perms));
-	}
-
 # returns array of key=>value
-# key = theme path - pipe delimited ("|"), e.g. Cars|German|VW
-# value = boolean - TRUE if permission to view (by default) or FALSE if denied (i.e. set via permission manager j* directive)
-#	--- will also update $current_user_collection_blacklisted_no_perms at the same time adding banned collections because of TRUE j<path> for current user
+# key = theme path, pipe delimited ("|"), e.g. Cars|German|VW
+# value = boolean - TRUE(1) if permission to view or FALSE if denied
+# --- set via permission manager j*, (include all), j<top level include> and j-<exclude below top level> directives
 #
 function getThemePathPerms() 
 	{	
@@ -41,9 +29,12 @@ function getThemePathPerms()
 			if (empty($item)) break;	// the current field is blank so quit field iteration 
 			if ($pathString != "") $pathString .= "|";	// only add separator if not first field
 			$pathString .= $item;									
-			if (array_search ("j${pathString}", $permissions)) 
+			if (
+					(substr_count ($pathString,"|") == 0 && !array_search ("j${pathString}", $permissions))		// for top level we just need to make sure that "jMyTheme" does not exist										
+					||
+					(array_search ("j-${pathString}", $permissions)) 	// look for minus path to indicate that we do not have permission from here and below
+				)
 				{				
-				array_push ($current_user_collection_blacklisted_no_perms, $ref);	// we have found a j* permission so add to black list.
 				$perm = false;		// *** IMPORTANT *** for this and all other sub-levels permission will not be granted - cool!
 				}
 			$stack[$pathString] = $perm;	// add to return stack
