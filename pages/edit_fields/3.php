f8e2e069a8cf5c7e0a51639b17914a0ffc5a1df7
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2013-09-15 02:39:24 +0000

    added hooks replacedropdowndefault and adjustdropdownoptiontrans
    git-svn-id: http://svn.montala.net/svn/resourcespace@4908 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/edit_fields/3.php
+++ pages/edit_fields/3.php
@@ -13,12 +13,15 @@ for ($m=0;$m<count($options);$m++)
 	}
 if ($auto_order_checkbox) {asort($option_trans);}	
 
+$adjusted_dropdownoptiontrans=hook("adjustdropdownoptiontrans","edit",array($field,$option_trans));
+if ($adjusted_dropdownoptiontrans){$option_trans=$adjusted_dropdownoptiontrans;}
+
 if (substr($value,0,1) == ',') { $value = substr($value,1); }	// strip the leading comma if it exists	
 
 ?><select class="stdwidth" name="<?php echo $name?>" id="<?php echo $name?>" <?php echo $help_js; ?>
 <?php if ($edit_autosave) {?>onChange="AutoSave('<?php echo $field["ref"] ?>');"<?php } ?>
 >
-<option value=""></option>
+<?php if (!hook("replacedropdowndefault","",array($field))){?><option value=""></option><?php } ?>
 <?php
 foreach ($option_trans as $option=>$trans)
 	{
