5f703ce61c52831f63a968badfb8e64d3bf6f884
tgleason <tgleason@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2013-09-16 15:28:50 +0000

    addthemeheadertoolaftername hook
    git-svn-id: http://svn.montala.net/svn/resourcespace@4913 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- pages/themes.php
+++ pages/themes.php
@@ -389,7 +389,7 @@ elseif ($themes_category_split_pages && !$theme_direct_jump)
 				}
 			}?>
 			<tr>
-			<td><div class="ListTitle"><a href="<?php echo $headerlink ?>" onClick="return CentralSpaceLoad(this,true);"><?php echo htmlspecialchars(i18n_get_translated(str_replace("*","",$headers[$n])))?></a></div></td>
+			<td><div class="ListTitle"><a href="<?php echo $headerlink ?>" onClick="return CentralSpaceLoad(this,true);"><?php echo htmlspecialchars(i18n_get_translated(str_replace("*","",$headers[$n])))?></a><?php hook('addthemeheadertoolaftername')?></div></td>
 			<td><div class="ListTools"><?php hook('addthemeheadertool')?><?php if (!hook("replacethemeselectlink")){?><a href="<?php echo $link ?>" onClick="return CentralSpaceLoad(this,true);">&gt;&nbsp;<?php echo $linklang;?></a><?php } 
 			if (checkperm("h") && $enable_theme_category_sharing) {?>&nbsp;&nbsp;<a href="<?php echo $sharelink ?>" onClick="return CentralSpaceLoad(this,true);">&gt;&nbsp;<?php echo $lang["share"]?></a><?php }
 			if ($enable_theme_category_edit && checkperm("t")) {?>&nbsp;&nbsp;<a href="<?php echo $editlink ?>" onClick="return CentralSpaceLoad(this,true);">&gt;&nbsp;<?php echo $lang["action-edit"]?></a><?php }
