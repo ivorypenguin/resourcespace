8b4b22d98fffd7b552ae96295b2b4af673bf57fe
hfrizen <hfrizen@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2013-10-08 22:29:25 +0000

    ADD: Added GreySpace styling to the latest additions.
    git-svn-id: http://svn.montala.net/svn/resourcespace@4955 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- include/version.php
+++ include/version.php
@@ -3,7 +3,6 @@ $productname="ResourceSpace"; # Product name. Do not change.
 $productversion="SVN";
 
 # External items reload key
-# Increment the below value to force a reload of the CSS and other included items.
-# This is intended for developer use when altering CSS. It will force a reload on all client browsers.
-$css_reload_key=60;
-?>
+# Increment the below value to force a reload of the CSS, JavaScripts and other included items.
+# This is intended for developer use when altering such files. It will force a reload on all client browsers.
+$css_reload_key=61;
