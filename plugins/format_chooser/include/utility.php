fadaaa58dbac6951ada5f881f4cd72c43dbc4d91
adoerfler <adoerfler@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2013-10-23 16:02:09 +0000

    BUG: format_chooser original size did not always work.
    * Ie. if the file had to be created, it failed.
    
    
    git-svn-id: http://svn.montala.net/svn/resourcespace@4990 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- plugins/format_chooser/include/utility.php
+++ plugins/format_chooser/include/utility.php
@@ -68,16 +68,20 @@ function getTargetFilename($ref, $ext, $size)
  */
 function getImageFormat($size)
 	{
+	if (empty($size))
+		return array('width' => 0, 'height' => 0);
+
 	$results = sql_query("select * from preview_size where id='" . escape_check($size) . "'");
 	if (empty($results))
-		die('Unknown size');
+		die('Unknown size: "' . $size . '"');
 	return $results[0];
 	}
 
 /**
  * Converts the file of the given resource to the new target file with the specified size. The
  * target file format is determined from the suffix of the target file.
- * The original colorspace of the image is retained.
+ * The original colorspace of the image is retained. If $width and $height are zero, the image
+ * keeps its original size.
  */
 function convertImage($resource, $page, $alternative, $target, $width, $height)
 	{
@@ -90,11 +94,14 @@ function convertImage($resource, $page, $alternative, $target, $width, $height)
 
 	$command .= " \"$originalPath\"[0] -auto-orient";
 
-	# Apply resize ('>' means: never enlarge)
-	$command .= " -resize $width";
-	if ($height > 0)
-		$command .= "x$height";
-	$command .= '>';
+	if ($width != 0 && $height != 0)
+		{
+		# Apply resize ('>' means: never enlarge)
+		$command .= " -resize \"$width";
+		if ($height > 0)
+			$command .= "x$height";
+		$command .= '>"';
+		}
 
 	$command .= " \"$target\"";
 
