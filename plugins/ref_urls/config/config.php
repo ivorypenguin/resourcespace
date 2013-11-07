df45af304dc5881cfae4d52ba8dcea3ec3188ba7
adoerfler <adoerfler@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2013-11-07 11:07:10 +0000

    ADD: ref_urls now has the option of allowing unauthorized access.
    * This can be used to speed up thumbnail retrieval on slow servers.
    * You can configure which images are allowed to be accessed without
      being logged in.
    
    
    git-svn-id: http://svn.montala.net/svn/resourcespace@5055 c08608d7-6e46-0410-86ca-f2a6f1370df5

---
+++ plugins/ref_urls/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+# You can configure images sizes that do not require a login. If you have a slow server,
+# testing the login for each thumbnail can be very slow.
+$unauthenticated_image_sizes=array('thm', 'col');
+
+?>
--- plugins/ref_urls/file.php
+++ plugins/ref_urls/file.php
@@ -4,7 +4,11 @@ $suppress_headers = true;
 
 include "../../include/db.php";
 include "../../include/general.php";
-include "../../include/authenticate.php";
+
+# Check if we need to authorize this access
+$size = getvalescaped('size', '');
+if (!is_array($unauthenticated_image_sizes) || !in_array($size, $unauthenticated_image_sizes))
+	include "../../include/authenticate.php";
 
 $ref = getvalescaped('ref', 0, true);
 $extension = getvalescaped('ext', 'jpg');
@@ -15,7 +19,6 @@ if ($ref == 0 || $extension == 'php')
 	exit;
 	}
 
-$size = getvalescaped('size', '');
 $alternative = getvalescaped('alternative', -1, true);
 $watermark = getvalescaped('watermark', false);
 $page = getvalescaped('page', 1, true);
