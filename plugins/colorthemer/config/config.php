8c82c29c9d2167fa1f989e3d0c08ac96674c9f3a
Aurangzeb <aureagle@gmail.com>
2016-04-23 00:33:03 +0500

    removing unused (obsoleted) files from old repository

--- plugins/colorthemer/config/config.php
+++
@@ -1,2 +0,0 @@
-<?php 
-$userfixedtheme=1;
--- plugins/greyspace/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-#$userfixedtheme="greyspace";
-global $available_themes;
-$available_themes[]="greyspace";
-
-$header_favicon="plugins/greyspace/gfx/interface/favicon.png";
